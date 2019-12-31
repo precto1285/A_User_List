35a7644e6f149526b98508a322f360d492dba0e6
Philippe Recto <precto1285@gmal.com>
2019-12-31 15:57:18 -0800

    created event listener for button

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ const http = new userHTTP;
 
 // Get Data from external API source:
 http.get('https://jsonplaceholder.typicode.com/users').then(data => console.log(data)).catch(err => console.log(err));
+
+// Add Event Listern for button
+const button = document.querySelector(".btn");
+button.addEventListener('click', (e) => { console.log('clicked'); e.preventDefault(); })
