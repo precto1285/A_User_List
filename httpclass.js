8edb501e326eb4076f3306054999884bc70f3d6e
Philippe Recto <precto1285@gmal.com>
2019-12-31 20:09:05 -0800

    created HTTP Post request

--- httpclass.js
+++ httpclass.js
@@ -1,8 +1,22 @@
-// Class to GET data.
+// Create a class for HTTP requests
 class userHTTP {
+  //  Make HTTP Get Request
   async get(url) {
     const response = await fetch(url);
     const resData = await response.json();
     return resData;
   }
+
+  // Make HTTP Post Request
+  async post(url, data) {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify(data)
+    });
+
+    const resData = await response.json();
+    return resData;
+  }
+
 }
